1f3c7da60763ec8401f8c9f6120c9155b289fe19
yjt-1997 <jiangtap1997qqq@qq.com>
2019-07-13 23:55:52 +0800

    [YJT] pass all AC of Story1

--- src/test/java/com/thoughtworks/tdd/ParkingBoyTest.java
+++ src/test/java/com/thoughtworks/tdd/ParkingBoyTest.java
@@ -34,40 +34,39 @@ public class ParkingBoyTest {
         //when
         Ticket ticket = parkingBoy.parkCar(car);
         //then
-        assertEquals(car,parkingLot.fetchCar(ticket));
+        assertEquals(car, parkingLot.fetchCar(ticket));
     }
 
     @Test
-    public void should_return_right_Car_when_fetchCar_given_right_Ticket(){
+    public void should_return_right_Cars_when_fetchCar_given_right_Tickets() {
         //given
         ParkingLot parkingLot = new ParkingLot();
         ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
-
         //when
         Car car1 = new Car();
         Ticket ticket1 = parkingBoy.parkCar(car1);
         Car car2 = new Car();
         Ticket ticket2 = parkingBoy.parkCar(car2);
         //then
-        assertEquals(car1,parkingLot.fetchCar(ticket1));
-        assertEquals(car2,parkingLot.fetchCar(ticket2));
+        assertEquals(car1, parkingLot.fetchCar(ticket1));
+        assertEquals(car2, parkingLot.fetchCar(ticket2));
     }
 
     @Test
-    public void should_return_null_when_fetchCar_given_null(){
+    public void should_return_null_when_fetchCar_given_null() {
         //given
         ParkingLot parkingLot = new ParkingLot();
         ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
-
         //when
         Car car1 = new Car();
         Ticket ticket1 = parkingBoy.parkCar(car1);
+        Car car = parkingBoy.fetchCar(null);
         //then
-        assertNull(parkingBoy.fetchCar(null));
+        assertNull(car);
     }
 
     @Test
-    public void should_return_null_when_fetchCar_given_usedTicket(){
+    public void should_return_null_when_fetchCar_given_usedTicket() {
         //given
         ParkingLot parkingLot = new ParkingLot();
         ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
@@ -75,7 +74,24 @@ public class ParkingBoyTest {
         Car car1 = new Car();
         Ticket ticket1 = parkingBoy.parkCar(car1);
         parkingBoy.fetchCar(ticket1);
+        Car result = parkingBoy.fetchCar(ticket1);
         //then
-        assertNull(parkingBoy.fetchCar(ticket1));
+        assertNull(result);
+    }
+
+    @Test
+    public void should_return_null_when_parkCar_given_the_full_parkinglot() {
+        //given
+        ParkingLot parkingLot = new ParkingLot(2);
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
+        //when
+        Car car1 = new Car();
+        Car car2 = new Car();
+        Car car3 = new Car();
+        parkingBoy.parkCar(car1);
+        parkingBoy.parkCar(car2);
+        Ticket result = parkingBoy.parkCar(car3);
+        //then
+        assertNull(parkingBoy.fetchCar(result));
     }
 }
